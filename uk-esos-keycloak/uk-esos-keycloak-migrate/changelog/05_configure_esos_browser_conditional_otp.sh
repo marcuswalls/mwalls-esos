323b9bbb3dfa6479f4301bc8b75d0b14f0bdd43e
Marcus Walls <marcus@mwalls.org>
2025-06-26 10:00:11 +0100

    Fix issue whereby username and password form became last item in the flow after updating OTP conditional flag

--- uk-esos-keycloak/uk-esos-keycloak-migrate/changelog/05_configure_esos_browser_conditional_otp.sh
+++ uk-esos-keycloak/uk-esos-keycloak-migrate/changelog/05_configure_esos_browser_conditional_otp.sh
@@ -19,19 +19,18 @@ GET_ESOS_BROWSER_EXECUTIONS=$(curl -s -L -X GET "$UPDATE_REALM_URL$UK_ESOS_REALM
 
 CONDITIONAL_OTP_ID=$(echo "$GET_ESOS_BROWSER_EXECUTIONS" | jq -r '.[] | select(.displayName=="ESOSBrowser Browser - Conditional OTP").id')
 
+CONDITIONAL_OTP_EXECUTION_OBJECT=$(echo "$GET_ESOS_BROWSER_EXECUTIONS" | jq --arg targetId "$CONDITIONAL_OTP_ID" '.[] | select(.id == $targetId) | .requirement = "REQUIRED"')
+
 #Update Conditional OTP execution
 CONDITIONAL_OTP_EXECUTION=$(curl -s -L -X PUT "$UPDATE_REALM_URL$UK_ESOS_REALM_NAME"/authentication/flows/"$ESOS_BROWSER"/executions \
 -H 'Content-Type: application/json' \
 -H "Authorization: Bearer $KEYCLOAK_ADMIN_ACCESS_TOKEN" \
---data-raw '{
-    "id": "'"$CONDITIONAL_OTP_ID"'",
-    "requirement": "REQUIRED"
-}')
+--data-raw "$CONDITIONAL_OTP_EXECUTION_OBJECT")
 
-if [ -z "$CONDITIONAL_OTP_EXECUTION" ]
+if echo "$CONDITIONAL_OTP_EXECUTION" | jq -e '.error' >/dev/null 2>&1;
 then
 	#In case of error during realm creation, print the error and exit in order to avoid successfully loging the script execution
-	echo " Realm $UK_ESOS_REALM_NAME updated failed"
+	echo " Realm $UK_ESOS_REALM_NAME update failed: $CONDITIONAL_OTP_EXECUTION"
 	exit;
 else
 	echo " Realm $UK_ESOS_REALM_NAME updated successfully"
